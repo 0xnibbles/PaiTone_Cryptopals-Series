9363b7b80f1fe37b511080c75cf8b5045a13850b
s4nkx0k <eduardommsi@gmail.com>
2019-12-26 23:37:02 +0000

    add an approach to char frequency

---
+++ set1/challenge3.py
@@ -0,0 +1,66 @@
+import base64
+
+def raw_bytes(input): 
+    
+    bytes = ''
+
+    for byte in zip(input[0::2], input [1::2]): # read a byte from input
+        bytes += chr(int(''.join(byte),16))
+        
+    return bytes
+
+def bytes2b64(bytes):
+
+    b64encode = base64.b64encode(str.encode(bytes))
+
+    return b64encode
+
+def charFrequency(letter):
+
+    frequencyTable ={
+        'E' : 12.0,
+        'T' : 9.10,
+        'A' : 8.12,
+        'O' : 7.68,
+        'I' : 7.31,
+        'N' : 6.95,
+        'S' : 6.28,
+        'R' : 6.02,
+        'H' : 5.92,
+        'D' : 4.32,
+        'L' : 3.98,
+        'U' : 2.88,
+        'C' : 2.71,
+        'M' : 2.61,
+        'F' : 2.30,
+        'Y' : 2.11,
+        'W' : 2.09,
+        'G' : 2.03,
+        'P' : 1.82,
+        'B' : 1.49,
+        'V' : 1.11,
+        'K' : 0.69,
+        'X' : 0.17,
+        'Q' : 0.11,
+        'J' : 0.10,
+        'Z' : 0.07
+    }
+    letterFrequency = frequencyTable.get(letter)
+    return letterFrequency
+
+#def singleXOR
+
+def main():
+    input = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
+
+    bytes = raw_bytes(input)
+
+    
+
+    #b64Input = bytes2b64(bytes)
+
+    
+
+
+if __name__ == "__main__":
+    main()
