fb3345786d348997b9a00cc5fa33f17982e57407
G2 <eduardommsi@gmail.com>
2020-09-16 00:56:16 +0100

    trying using hamming distance. not working

--- set1/challenge8.py
+++ set1/challenge8.py
@@ -1,34 +1,62 @@
 import base64
+from challenge3 import stringScore
 from challenge6 import hammingDistance
 import pprint
 
 
 #testing approach
-def detect_AES_ECB(ciphertext):
+def detect_AES_ECB(ciphertext_list):
 
     repeated_Blocks = {}
 
+    ct_blocks = {}
     #print(ciphertext)
     k=0
-    for i in range(0,len(ciphertext),16):
-        block = ciphertext[i:i+16]
-        #print(block)
-        if block in repeated_Blocks:
-            repeated_Blocks[block] +=1
-        else:
-            repeated_Blocks[block] = 1
+    j= -1
     
-    pprint.pprint(repeated_Blocks)
+    for ct in ciphertext_list:
+        j=j+1
+        ct_blocks[j] = 0
+        ct_score = 0
+        s1 = b""
+        s2 = b""
+        for i in range(0,len(ct),16):
+            block = ct[i:i+16]
+            #print(block)
+            '''
+            if block in repeated_Blocks:
+                repeated_Blocks[block] +=1
+            else:
+                repeated_Blocks[block] = 1
+            '''
+            s2 = block
+            #print(s1)
+            if s1 != b"":
+                #ct_score += stringScore(block)
+                ct_score += hammingDistance(s1,s2) # less distance = more equal
+                #print(ct_score)
+
+            s1 = s2
+
+        ct_blocks[j] = ct_score
+
+    ct_ECB_index = sorted(ct_blocks, key=lambda x: ct_blocks[x], reverse=True)[0]
+    print(ciphertext_list[ct_ECB_index])
+    #print(sorted(ct_blocks, key=lambda x: ct_blocks[x], reverse=True))
 
 def main():
     input =""
-    ciphertext= ""
-    with open("7.txt") as file:
+    ciphertext= []
+    with open("8.txt") as file:
         for line in file:
-            input += line.strip("\n")
-    ciphertext = input.encode()
-    #print(ciphertext)
+            ciphertext.append(line.strip("\n").encode())
+        #content = [line.strip("\n") for line in file]
+    #print(content)
+    #ciphertext = input.encode()
+    #pprint.pprint(ciphertext[:12])
     detect_AES_ECB(ciphertext)
+
+    
 
 
 if __name__ == "__main__":
