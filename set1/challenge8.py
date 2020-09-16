0be725cafc382837c141153fc173a0fdca15fa9e
G2 <eduardommsi@gmail.com>
2020-09-17 00:21:58 +0100

    set 1 challenge 8 syntax and comments fixes

--- set1/challenge8.py
+++ set1/challenge8.py
@@ -2,7 +2,7 @@
 '''
     According to the ECB mode, if there are identical blocks of plaintext, these will result in
     identical ciphertext blocks. So, the approach here is to detect which is the ciphertext
-    where exists identical ou equal blocks.
+    where exists identical or equal blocks.
     The ciphertext with more equal blocks is more likely to be encrypted with AES-ECB
 '''
 
@@ -14,7 +14,7 @@ def detect_AES_ECB(ciphertext_list):
 
         ct = ciphertext_list[i] # selecting a ciphertext from the list
         
-        repeated_Blocks = []    # list to save repeated block of each ciphertext
+        repeated_Blocks = []    # list to save repeated blocks of each ciphertext
         ct_score = 0            # number of repeated blocks
         for j in range(0,len(ct),16):   # chunks of 16 bytes
             block = ct[j:j+16]          #  cyphertext block of 16 bytes
@@ -28,7 +28,6 @@ def detect_AES_ECB(ciphertext_list):
 
     ct_ECB_index = sorted(ct_blocks, key=lambda x: ct_blocks[x], reverse=True)[0] # sorting for the index with the most block reptitions in reverse order
     return ct_ECB_index, ct_blocks[ct_ECB_index]
-        
 
 
 def main():
@@ -42,9 +41,6 @@ def main():
     aes_ecb_index, ecb_score = detect_AES_ECB(ciphertext)
     print("\nThe ciphertex encrypted with AES-ECB is:\n\n"+ciphertext[aes_ecb_index].decode())
     print("\nRepeated blocks: "+str(ecb_score))
-
-    
-
 
 if __name__ == "__main__":
     main()
