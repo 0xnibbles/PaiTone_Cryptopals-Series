81de1ab1101afb506c52c1048aca990133438324
eduardos <eduardo@EduardoS-Desktop.dm.cx>
2020-09-16 18:15:36 +0100

    using repeated block in a list. Testing

--- set1/challenge8.py
+++ set1/challenge8.py
@@ -7,42 +7,35 @@ import pprint
 #testing approach
 def detect_AES_ECB(ciphertext_list):
 
-    repeated_Blocks = {}
-
     ct_blocks = {}
-    #print(ciphertext)
-    k=0
-    j= -1
-    
-    for ct in ciphertext_list:
-        j=j+1
+    for j in range(0,len(ciphertext_list)):
+        print(j)
+        print("alala")
         ct_blocks[j] = 0
-        ct_score = 0
-        s1 = b""
-        s2 = b""
-        for i in range(0,len(ct),16):
-            block = ct[i:i+16]
-            #print(block)
-            '''
-            if block in repeated_Blocks:
-                repeated_Blocks[block] +=1
-            else:
-                repeated_Blocks[block] = 1
-            '''
-            s2 = block
-            #print(s1)
-            if s1 != b"":
-                #ct_score += stringScore(block)
-                ct_score += hammingDistance(s1,s2) # less distance = more equal
-                #print(ct_score)
+        for ct in ciphertext_list:
+            repeated_Blocks = []
+            ct_score = 0
+            for i in range(0,len(ct),16):
+                block = ct[i:i+16]
+                #print(block)
+                
+                if block in repeated_Blocks:
+                    #repeated_Blocks[block] +=1
+                    ct_score += 1
+                    ct_blocks[j] +=1
+                    print("entraaaaaaaaaa")
+                    print(ct_blocks[j])
+                else:
+                    repeated_Blocks.append(block)
+        #ct_blocks[j] = ct_score
+        #print(ct_score)
+        #print(ct_blocks)
 
-            s1 = s2
+    #ct_ECB_index = sorted(ct_blocks, key=lambda x: ct_blocks[x], reverse=True)[0]
+    #print(ciphertext_list[ct_ECB_index])
+    print(ct_blocks)
+        
 
-        ct_blocks[j] = ct_score
-
-    ct_ECB_index = sorted(ct_blocks, key=lambda x: ct_blocks[x], reverse=True)[0]
-    print(ciphertext_list[ct_ECB_index])
-    #print(sorted(ct_blocks, key=lambda x: ct_blocks[x], reverse=True))
 
 def main():
     input =""
